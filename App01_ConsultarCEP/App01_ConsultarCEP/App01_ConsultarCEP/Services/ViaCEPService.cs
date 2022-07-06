93dfa1515ae0a20f0c6b6055d3e51d6e34bd05e5
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-07-06 11:14:23 -0300

    Limpando pasta e criando App01

---
+++ App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/Services/ViaCEPService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App01_ConsultarCEP.Services
+{
+    public class ViaCEPService
+    {
+        private static string EnderecoUrl = "https://viacep.com.br/ws/{0}/json/";
+    }
+}
