32affe9fc24f55d78f6c31f95eb4761f94dbdf99
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-07-07 10:25:41 -0300

    updating files

---
+++ App01_ConsultaCEP/App01_ConsultaCEP/App01_ConsultaCEP/Services/Model/Address.cs
@@ -0,0 +1,14 @@
+﻿namespace App01_ConsultaCEP.Services.Model
+{
+    public class Address
+    {
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Localidade { get; set; }
+        public string Uf { get; set; }
+        public string Ibge { get; set; }
+        public string Gia { get; set; }
+    }
+}
---
+++ App01_ConsultaCEP/App01_ConsultaCEP/App01_ConsultaCEP/Services/ViaCEPService.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using Newtonsoft.Json;
+using App01_ConsultaCEP.Services.Model;
+
+namespace App01_ConsultaCEP.Services
+{
+    public class ViaCEPService
+    {
+        private static string EnderecoUrl = "https://viacep.com.br/ws/{0}/json/";
+
+        public static Address FindAddressByCep(string cep)
+        {
+            string novoEnderecoUrl = string.Format(EnderecoUrl, cep);
+
+            WebClient wc = new WebClient();
+            string conteudo = wc.DownloadString(novoEnderecoUrl);
+
+            Address address = JsonConvert.DeserializeObject<Address>(conteudo);
+            return address;
+        }
+    }
+}
