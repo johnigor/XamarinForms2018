ae87d17f722a176edfc929b205bcfef84d1ec32e
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-07-07 11:42:25 -0300

    Método BuscarCEP

--- App01_ConsultaCEP/App01_ConsultaCEP/App01_ConsultaCEP/MainPage.xaml.cs
+++ App01_ConsultaCEP/App01_ConsultaCEP/App01_ConsultaCEP/MainPage.xaml.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
+using App01_ConsultaCEP.Services;
+using App01_ConsultaCEP.Services.Model;
 
 namespace App01_ConsultaCEP
 {
@@ -13,6 +10,16 @@ namespace App01_ConsultaCEP
         public MainPage()
         {
             InitializeComponent();
+
+            BOTAO.Clicked += BuscarCEP;
+        }
+
+        private void BuscarCEP(object sender, EventArgs args)
+        {
+            string cep = CEP.Text.Trim();
+            Address address = ViaCEPService.FindAddressByCep(cep);
+
+            RESULTADO.Text = string.Format("Endereço: {0}, {1}, {2}", address.Localidade, address.Uf, address.Logradouro);
         }
     }
 }
