8b596de22a3590a0b23468b01630c6a3c1179691
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-07-08 11:53:55 -0300

    Creating project Layouts

--- App02_TipoPaginaXF/App02_TipoPaginaXF/App02_TipoPaginaXF/PageType/Master/Master.xaml.cs
+++ App02_TipoPaginaXF/App02_TipoPaginaXF/App02_TipoPaginaXF/PageType/Master/Master.xaml.cs
@@ -20,16 +20,19 @@ namespace App02_TipoPaginaXF.PageType.Master
         private void ChangePage1(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Navigation.Page1());
+            IsPresented = false;
         }
 
         private void ChangePage2(object sender, EventArgs args)
         {
             Detail = new Navigation.Page2();
+            IsPresented = false;
         }
 
         private void ChangePage3(object sender, EventArgs args)
         {
             Detail = new Content();
+            IsPresented = false;
         }
     }
 }
