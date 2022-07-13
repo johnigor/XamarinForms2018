e66640a0b2d3f59812d8f9216ac9d9deb8a2d6df
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-07-13 11:48:15 -0300

    Image - XAML

--- Section08/App01_ControleXF/App01_ControleXF/App01_ControleXF/Menu/Master.xaml.cs
+++ Section08/App01_ControleXF/App01_ControleXF/App01_ControleXF/Menu/Master.xaml.cs
@@ -72,5 +72,10 @@ namespace App01_ControleXF.Menu
         {
             Detail = new Controles.SwitchPage();
         }
+        
+        private void GoImagePage(object sender, EventArgs args)
+        {
+            Detail = new Controles.ImagePage();
+        }
     }
 }
