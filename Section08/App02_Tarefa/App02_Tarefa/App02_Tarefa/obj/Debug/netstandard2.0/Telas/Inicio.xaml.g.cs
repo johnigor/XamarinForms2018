8d0f28637a1f48a8fc2e9b58ed1616ce4e1e1ebe
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-07-13 17:58:26 -0300

    Página "Início"

--- Section08/App02_Tarefa/App02_Tarefa/App02_Tarefa/obj/Debug/netstandard2.0/Telas/Inicio.xaml.g.cs
+++ Section08/App02_Tarefa/App02_Tarefa/App02_Tarefa/obj/Debug/netstandard2.0/Telas/Inicio.xaml.g.cs
@@ -17,8 +17,12 @@ namespace App02_Tarefa.Telas {
     public partial class Inicio : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.StackLayout SL_Tarefas;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(Inicio));
+            SL_Tarefas = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.StackLayout>(this, "SL_Tarefas");
         }
     }
 }
