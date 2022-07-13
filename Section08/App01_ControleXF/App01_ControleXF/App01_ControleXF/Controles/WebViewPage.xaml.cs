24256a6525c4efcfe8cef8d12c6c11d64d84a654
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-07-13 16:34:07 -0300

    Corrigindo falhas
    Menu lateral Android
    ScrollView para os controles
    Toolbar em todas as paginas

---
+++ Section08/App01_ControleXF/App01_ControleXF/App01_ControleXF/Controles/WebViewPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App01_ControleXF.Controles
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WebViewPage : ContentPage
+    {
+        public WebViewPage()
+        {
+            InitializeComponent();
+        }
+        public void GoPage(object sender, EventArgs args)
+        {
+            Navegador.Source = EnderecoSite.Text;
+        }
+        public void GoProximo(object sender, EventArgs args)
+        {
+            if (Navegador.CanGoForward) Navegador.GoForward();
+        }
+        public void GoVoltar(object sender, EventArgs args)
+        {
+            if (Navegador.CanGoBack) Navegador.GoBack();
+        }
+        public void ActionCarregando(object sender, EventArgs args)
+        {
+            LblStatus.Text = "Carregando...";
+        }
+        public void ActionCarregado(object sender, EventArgs args)
+        {
+            LblStatus.Text = "Finalizado.";
+        }
+    }
+}
