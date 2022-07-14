193c8299ebf529bb0172401734ba63c6c982a89b
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-07-14 15:10:18 -0300

    Metodos de selecao de prioridade

--- Section08/App02_Tarefa/App02_Tarefa/App02_Tarefa/Telas/Cadastro.xaml.cs
+++ Section08/App02_Tarefa/App02_Tarefa/App02_Tarefa/Telas/Cadastro.xaml.cs
@@ -16,5 +16,20 @@ namespace App02_Tarefa.Telas
         {
             InitializeComponent();
         }
+        public void PrioridadeSelectAction(object sender, EventArgs args)
+        {
+            var stacks = SL_Prioridades.Children;
+
+            foreach (var linha in stacks)
+            {
+                Label LblPrioridade = ((StackLayout)linha).Children[1] as Label;
+                LblPrioridade.TextColor = Color.Gray;
+            }
+
+            ((Label)((StackLayout)sender).Children[1]).TextColor = Color.Black;
+            FileImageSource source = ((Image)((StackLayout)sender).Children[0]).Source as FileImageSource;
+            String prioridade = TxtNome.Text = source.File.ToString().Replace("Resources/", "").Replace(".png", "");
+            TxtNome.Text = prioridade;
+        }
     }
 }
