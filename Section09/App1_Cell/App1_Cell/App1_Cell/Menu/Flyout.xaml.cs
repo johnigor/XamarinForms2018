50942421f24e2297d2edb816ce7dac4ca838ee1b
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-07-20 16:29:02 -0300

    Creating a new project

---
+++ Section09/App1_Cell/App1_Cell/App1_Cell/Menu/Flyout.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1_Cell.Menu
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Flyout : FlyoutPage
+    {
+        public Flyout()
+        {
+            InitializeComponent();
+        }
+        public void GoPage1(object sender, EventArgs args)
+        {
+            //Detail = new NavigationPage(new GoPage1());
+        }
+    }
+}
