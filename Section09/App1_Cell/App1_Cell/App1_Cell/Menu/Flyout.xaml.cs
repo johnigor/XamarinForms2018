ff96d0863fe2e075f61cc6d145030b581a8f17c7
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-07-25 17:23:40 -0300

    Correcoes

--- Section09/App1_Cell/App1_Cell/App1_Cell/Menu/Flyout.xaml.cs
+++ Section09/App1_Cell/App1_Cell/App1_Cell/Menu/Flyout.xaml.cs
@@ -14,30 +14,37 @@ namespace App1_Cell.Menu
         public void GoPageText(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Pagina.TextCellPage());
+            IsPresented = false;
         }
         public void GoPageImage(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Pagina.ImageCellPage());
+            IsPresented = false;
         }
         public void GoPageEntry(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Pagina.EntryCellPage());
+            IsPresented = false;
         }
         public void GoPageSwitch(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Pagina.SwitchCellPage());
+            IsPresented = false;
         }
         public void GoPageView(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Pagina.ViewCellPage());
+            IsPresented = false;
         }
         public void GoPageListView(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Pagina.ListViewPage());
+            IsPresented = false;
         }
         public void GoPageListViewButton(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Pagina.ListViewButtonPage());
+            IsPresented = false;
         }
     }
 }
