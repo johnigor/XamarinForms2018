212166960d8a4b6b2807b76df652cba8c84a26d8
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-07-26 17:05:03 -0300

    Creating project Cinemix

--- Section10/App01_Styles/App01_Styles/App01_Styles/Menu/Flyout.xaml.cs
+++ Section10/App01_Styles/App01_Styles/App01_Styles/Menu/Flyout.xaml.cs
@@ -15,23 +15,27 @@ namespace App01_Styles.Menu
         private void GoImplicitStyle(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Page.ImplicitStylePage());
+            IsPresented = false;
         }
-
         private void GoExplicitStyle(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Page.ExplicitStylePage());
+            IsPresented = false;
         }
         private void GoGlobalStyle(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Page.GlobalStylePage());
+            IsPresented = false;
         }
         private void GoInheritStyle(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Page.InheritStylePage());
+            IsPresented = false;
         }
         private void GoDynamicStyle(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Page.DynamicStylePage());
+            IsPresented = false;
         }
     }
 }
