e1f331ce6b6ee1262ec11fefccce99a7f59ec3ca
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-07-29 15:10:59 -0300

    Setting database with DependencyService

---
+++ Section11/App01_Vagas/App01_Vagas/App01_Vagas/Data/BancoContext.cs
@@ -0,0 +1,44 @@
+﻿using SQLite;
+using Xamarin.Forms;
+using App01_Vagas.Models;
+using System.Collections.Generic;
+
+namespace App01_Vagas.Data
+{
+    internal class BancoContext
+    {
+        private readonly SQLiteConnection _connection;
+
+        public BancoContext()
+        {
+            var dependencyService = DependencyService.Get<ICaminho>();
+            string path = dependencyService.GetPath("Database.Sqlite");
+            _connection = new SQLiteConnection(path);
+        }
+
+        public List<Vaga> Consultar(Vaga vaga)
+        {
+            return null;
+        }
+
+        public Vaga ObterVagaPorId()
+        {
+            return null;
+        }
+
+        public void Cadastrar(Vaga vaga)
+        {
+
+        }
+
+        public void Atualizar(Vaga vaga)
+        {
+
+        }
+
+        public void Excluir(int id)
+        {
+
+        }
+    }
+}
---
+++ Section11/App01_Vagas/App01_Vagas/App01_Vagas/Data/ICaminho.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App01_Vagas.Data
+{
+    interface ICaminho
+    {
+        string GetPath(string DatabaseName);
+    }
+}
