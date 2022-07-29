b01ef8efe2cd42f10919c1f245f15b52f6b20e65
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-07-29 13:23:53 -0300

    Creating Model "Vaga"

---
+++ Section11/App01_Vagas/App01_Vagas/App01_Vagas/Models/Vaga.cs
@@ -0,0 +1,14 @@
+﻿namespace App01_Vagas.Models
+{
+    internal class Vaga
+    {
+        public string Cargo { get; set; }
+        public short Quantidade { get; set; }
+        public string Cidade { get; set; }
+        public double Salario { get; set; }
+        public string Descricao { get; set; }
+        public string TipoContratacao { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+    }
+}
